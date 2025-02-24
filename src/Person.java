9e46b08d5644a4d657e49117a6fb24f3ae4894d7
ThaoPham372 <tuongthao02101992@gmail.com>
2025-02-24 12:44:09 +0700

    Create Object

---
+++ src/Person.java
@@ -0,0 +1,61 @@
+public abstract class Person {
+    // Attribute
+    protected String id;
+    protected String name;
+    protected String email;
+    protected String phone;
+    protected String password;
+
+    // Constructor
+    public Person(String id, String name, String email, String phone, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    // Getter
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setter
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
---
+++ src/User.java
@@ -0,0 +1,35 @@
+import java.util.List;
+
+public class User extends Person{
+    private List<String> borrowBooks;
+    private int maxBookAllowed = 5;
+
+    public User(String id, String name, String email, String phone, String password) {
+        super(id, name, email, phone, password);
+    }
+
+    // Constructor
+    public User(String id, String name, String email, String phone, String password, List<String> borrowBooks, int maxBookAllowed) {
+        super(id, name, email, phone, password);
+        this.borrowBooks = borrowBooks;
+        this.maxBookAllowed = maxBookAllowed;
+    }
+
+    // Getter
+    public List<String> getBorrowBooks() {
+        return borrowBooks;
+    }
+
+    public int getMaxBookAllowed() {
+        return maxBookAllowed;
+    }
+
+    //Setter
+    public void setBorrowBooks(List<String> borrowBooks) {
+        this.borrowBooks = borrowBooks;
+    }
+
+    public void setMaxBookAllowed(int maxBookAllowed) {
+        this.maxBookAllowed = maxBookAllowed;
+    }
+}
